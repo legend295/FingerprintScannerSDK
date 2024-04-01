0dce9e0c2b8734b1f456238e3220cdc380d5f823
Harry <rattansingh1699@gmail.com>
2024-04-01 12:45:00 +0530

    implementation of library added

--- app/src/main/java/com/fingerprintscanner/MainActivity.kt
+++ app/src/main/java/com/fingerprintscanner/MainActivity.kt
@@ -2,10 +2,12 @@ package com.fingerprintscanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.scanner.ArithmaticsHelper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ArithmaticsHelper().add(4, 4)
     }
 }
